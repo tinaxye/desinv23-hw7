7a8e8936548926739f801f38be9c8a12fb2f48d1
Tina Ye <tinaye@berkeley.edu>
2018-03-05 22:49:04 -0800

    Create wrong-turn.ino

---
+++ wrong-turn.ino
@@ -0,0 +1,24 @@
+//choose your pin
+int pin = 9;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 9 as an output.
+  pinMode(pin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    digitalWrite(pin, HIGH);
+    delay(2000);
+    digitalWrite(pin, LOW);
+    delay(200);
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(1000);
+ }
