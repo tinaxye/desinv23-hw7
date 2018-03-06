b21c9ed8946ac13cedb5b91d325530561688e840
Tina Ye <tinaye@berkeley.edu>
2018-03-05 22:52:49 -0800

    Create meeting.ino

---
+++ meeting.ino
@@ -0,0 +1,36 @@
+//choose your pin
+int pin = 9;
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+  // initialize digital pin 9 as an output.
+  pinMode(pin, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+    
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(200);
+    
+    digitalWrite(pin, HIGH);
+    delay(200);
+    digitalWrite(pin, LOW);
+    delay(3000);
+ }
